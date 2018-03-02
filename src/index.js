da473712b827126cb82d28cfeccc163d1a849768
poulkud <poulkud@gmail.com>
2018-03-02 18:51:46 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var chars = str.split(''),
+      stack = [],
+      open = ['{', '(', '['],
+      close = ['}', ')', ']'],
+      openBracket,closeBracket;
+  for (var i = 0, len = chars.length; i < len; i++) {
+         openBracket = open.indexOf(chars[i]);
+         if (openBracket !== -1) {
+            stack.push(openBracket);
+            continue;
+         }
+         closeBracket = close.indexOf(chars[i]);
+         if (closeBracket !== -1) {
+          openBracket = stack.pop();
+             if (closeBracket !== openBracket) {
+                 return false;
+               }
+             }
+           }
+  if (stack.length !== 0) {
+    return false;
+      }
+  return true;
+    }
